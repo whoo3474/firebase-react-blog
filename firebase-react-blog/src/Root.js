477ba4124faf45ab28485be4081d5c9d52613ee7
JeongMin Woo <minhan_a@naver.com>
2019-01-09 22:46:39 +0900

    develop : store/index.js createStore

--- firebase-react-blog/src/Root.js
+++ firebase-react-blog/src/Root.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom'
-import App from 'components/App';
+import App from './components/App';
 
 class Root extends Component {
     render() {
---
+++ firebase-react-blog/src/store/index.js
@@ -0,0 +1,16 @@
+import { default as contact} from './modules/contact';
+import { default as portfolio} from './modules/portfolio';
+import { default as timeLine} from './modules/timeLine';
+import thunk from 'redux-thunk';
+
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+
+const reducers = combineReducers({
+    contact,
+    portfolio,
+    timeLine
+});
+
+const store = createStore(reducers,applyMiddleware(thunk));
+
+export default store;
